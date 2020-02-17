f02f910ed3b4e21333f9ac9bc204b6e6b7edde36
Shubham Sharma <SharmaShubham@SEASIAINFOTECH.COM>
2020-02-17 17:58:57 +0530

    project

---
+++ temp/clever/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+
+def index(request):
+    return render(request, 'clever/index.html', {})
+def htm(request):
+    return render(request, 'clever/htm.html', {})
